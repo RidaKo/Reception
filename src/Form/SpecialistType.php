8ff25cda2264523682f84394d2d5e8efb7427a44
RidaKo <ridas.kozukauskas@gmail.com>
2023-10-25 20:21:34 +0300

    Changed password and secret_key formatting to dots in forms

--- src/Form/SpecialistType.php
+++ src/Form/SpecialistType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Specialist;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,9 +15,9 @@ class SpecialistType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('secretKey')
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add('secretKey', PasswordType::class)
             ->add('register', SubmitType::class)
         ;
     }
