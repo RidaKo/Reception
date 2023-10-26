a8351c27eff5503be531f544ed1bde09dae159aa
RidaKo <ridas.kozukauskas@gmail.com>
2023-10-26 17:27:49 +0300

    Finished and fixed unittests functionality

--- tests/Service/HolidayApiTest.php
+++ tests/Service/HolidayApiTest.php
@@ -2,6 +2,8 @@
 
 namespace App\Tests\Service;
 
+use App\Service\HolidayApi;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class HolidayApiTest extends KernelTestCase
@@ -13,5 +15,8 @@ class HolidayApiTest extends KernelTestCase
         $this->assertSame('test', $kernel->getEnvironment());
         // $routerService = static::getContainer()->get('router');
         // $myCustomService = static::getContainer()->get(CustomService::class);
+        $holiday_api = static::getContainer()->get(HolidayApi::class);
+        
+        $this->assertTrue($holiday_api->checkIfReservationTimeIsHoliday(new DateTime('2022-02-16')));
     }
 }
